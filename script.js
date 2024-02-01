470b33f391cc81f654e3e0cf21e4231b8c575253
Ney Adrian <112216425+neyadrian@users.noreply.github.com>
2024-02-01 20:54:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var meuBotao = document.getElementById("meuBotao");
+  
+    meuBotao.addEventListener("click", function() {
+      alert("Obrigado pela visita :)");
+    });
+  });
+  
